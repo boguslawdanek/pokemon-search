2a84b721804d4d4bbea02899bf35a0efe267ca02
boguslawdanek <boguslawdanek@gmail.com>
2025-01-27 09:38:20 +0100

    added animated-pulse to loading skeleton

--- src/app/pokemon/[pokemonName]/loading.tsx
+++ src/app/pokemon/[pokemonName]/loading.tsx
@@ -3,8 +3,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <div className="flex flex-col space-y-4 items-center">
-      <Skeleton className="h-[36px] w-[200px]" />
-      <Skeleton className="h-[200px] w-[200px]" />
+      <Skeleton className="h-[36px] w-[200px] animate-pulse" />
+      <Skeleton className="h-[200px] w-[200px] animate-pulse" />
     </div>
   );
 }
